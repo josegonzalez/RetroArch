f4b8b9f4351b7a3981ca5c5157d95b791d60d8c5
Brandon T. Kowalski <brandon@kowalski.io>
2026-02-26 19:39:22 -0500

    Continued work on streamlined

--- pkg/android/phoenix-common/src/com/retroarch/browser/retroactivity/RetroActivityCommon.java
+++ pkg/android/phoenix-common/src/com/retroarch/browser/retroactivity/RetroActivityCommon.java
@@ -14,7 +14,7 @@
  */
 package com.retroarch.browser.retroactivity;
 
-import com.retroarch.BuildConfig;
+import com.retroarch.btk.BuildConfig;
 import com.retroarch.browser.preferences.util.UserPreferences;
 import com.retroarch.playcore.PlayCoreManager;
 
